06d5675e27fc093b58c1a0b57156484d357668b4
gizocco123 <105619713+gizocco123@users.noreply.github.com>
2023-01-22 21:34:30 -0600

    Delete pytech_update.py

--- pytech_update.py
+++
@@ -1,6 +0,0 @@
-students.find{()}
-MongoDB: update_one
-result = db.collection.update_one({"student_id": 1007}, {$"set": {"last_name": "John"}})
-MongoDB: find_one()
-doc = db.collection_student.find_one({"student_id": "1007"})
-print(doc["student_id"])
