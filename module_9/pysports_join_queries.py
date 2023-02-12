7de3f7aabcd4524ea0eec4cceb7000eabfbb8daf
gizocco123 <105619713+gizocco123@users.noreply.github.com>
2023-02-12 14:08:46 -0600

    Create pysports_join_queries.py

---
+++ module_9/pysports_join_queries.py
@@ -0,0 +1,11 @@
+SELECT player_id = 4, John, Ramos, Hawks
+FROM player
+INNER JOIN Hawks
+	ON John.4 = Hawks.67;
+    
+SELECT player_id = 5, Sam, Wiggins, Bears
+FROM player
+INNER JOIN Bears
+	ON Sam.5 = Bears.56
+    
+    
